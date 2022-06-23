23a1db75cbd9c95fd82019f76a7685bcf40741e9
Junjie Yu <junjieyu@icloud.com>
2022-06-22 23:44:37 -0700

    commit

--- script.js
+++ script.js
@@ -4,8 +4,36 @@
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
+let currentScore = 0;
 
 //staring conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
+
+// Rolling dice dunctionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display Dice
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true,
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0EL.textContent = currentScore;
+  } else {
+    // Switch to next play
+    currentScore = 0;
+    current0EL.textContent = currentScore;
+  }
+});
