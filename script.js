6fb6125a420f5bc8e2db323052b27f901300ba11
Junjie Yu <junjieyu@icloud.com>
2022-06-24 21:51:55 -0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEL = document.querySelector('.dice');
@@ -10,9 +12,12 @@ const btnHold = document.querySelector('.btn--hold');
 const current0EL = document.getElementById('current--0');
 const current1EL = document.getElementById('current--1');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //staring conditions
+
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
@@ -30,10 +35,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0EL.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next play
     currentScore = 0;
-    current0EL.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 });
